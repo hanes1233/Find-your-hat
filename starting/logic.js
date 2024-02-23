bed0671df7daf7f66802041f70cba93bdc75995d
Pavel <136936071+hanes1233@users.noreply.github.com>
2024-02-23 14:18:14 +0100

    Delete starting/logic.js

--- starting/logic.js
+++
@@ -1,8 +0,0 @@
-//const { time } = require('console');
-//const { randomBytes } = require('crypto');
-//const { path } = require('express/lib/application');
-//const { redirect } = require('express/lib/response');
-//const { read } = require('fs');
-//const prompt = require('prompt-sync')({sigint: 'y'});
-
-
